0c12ee7cc3cab4b3a818a5f8980dd8e1104cede8
root-folder <69852608+root-folder@users.noreply.github.com>
2020-08-18 14:42:41 +0200

    Add files via upload
    the program calculates the percentage mark of a pupil's scor

---
+++ instamark.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+line = "-" * 35
+chars = tuple('abcdefghijklmnopqrstyvwxyz`~@#$%^&*()_-=:;<,.>/?|')
+
+a = ('Distinction','A')
+b = ('Merit','B')
+c = ('Credit','C')
+d = ('Satisfactory', ' D')
+f = ('UnSatisfactory', 'F')
+
+def unpacker(values):
+	for item in values:
+		print(item)
+	print(line)
+
+def calculator(paper_mark = 0, pupil_mark = 0):
+	percent = (pupil_mark / paper_mark) * 100
+	return round(percent)
+
+def invalid_value():
+	print("Invalid value!")
+	print(line)
+
+def decider(percentage = 0):
+	if (percentage < 1) | (percentage > 100):
+		invalid_value()
+	elif percentage in chars:
+		invalid_value()
+	elif percentage >= 75:
+		print(str(percentage) + '%')
+		unpacker(a)
+	elif percentage >= 65:
+		print(str(percentage) + '%')
+		unpacker(b)
+	elif percentage >= 40:
+		print(str(percentage) + '%')
+		unpacker(c)
+	elif percentage >= 30:
+		print(str(percentage) + '%')
+		unpacker(d)
+	elif percentage < 30:
+		print(str(percentage) + '%')
+		unpacker(f)
+
+print(chars)
+try:
+	paper = int(input("Paper's Mark: "))
+
+	while True:
+		pupil = int(input("Pupil's Mark: "))
+		if pupil == 0:
+			print("Good Bye!")
+			break
+		if pupil in chars:
+			invalid_value()
+		decider(calculator(paper, pupil))
+except:
+	pass
